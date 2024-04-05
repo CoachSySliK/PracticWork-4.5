fdbc82cf4e0fdf05e13703a573edabdb0a4acdc1
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-06 00:04:07 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-4.5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Проверяем делится ли X на Y без остатка\n";
+    cout << "Введите X: ";
+    cin >> x;
+    cout << "Введите Y: ";
+    cin >> y;
+    cout << "----Проверяем----\n";
+    if (0 == x % y) {
+        cout << "Да, " << x << " делится на " << y << " без остатка.\n";
+    }
+    else {
+        cout << "Нет, " << x << " не делится на " << y << " без остатка.\n";
+    }
+    return 0;
+}
